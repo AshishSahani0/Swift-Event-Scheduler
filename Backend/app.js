c87c9d2aec7d3ba5de5f4f9f15f32295f3738e65
Ashish Sahani <ashishsahani0011@gmail.com>
2025-05-13 09:41:12 +0530

    app.ja

--- Backend/app.js
+++ Backend/app.js
@@ -1,15 +1,31 @@
 import express from "express";  
 import dotenv from "dotenv";
 import connectDB from "./database/db.js";
+import authRouter from "./routes/authRouter.js";
+import { errorMiddleware } from "./middlewares/errorMiddleware.js";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 export const app = express();
 
+app.use(cors({
+  origin: [process.env.FRONTEND_URL],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+}));
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 
+
+app.use("/api/v1/auth", authRouter);
 
 connectDB();
+app.use(errorMiddleware);
 
 
 export default app;
