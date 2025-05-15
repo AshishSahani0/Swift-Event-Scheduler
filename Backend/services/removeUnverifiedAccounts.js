49a0cfdec8906c0c381483e5b5c0fc0cce49c6d0
Ashish Sahani <ashishsahani0011@gmail.com>
2025-05-15 16:29:28 +0530

    removeUnverifiedAccounts

---
+++ Backend/services/removeUnverifiedAccounts.js
@@ -0,0 +1,17 @@
+import cron from "node-cron";
+import User from "../models/userModels.js";
+
+export const removeUnverifiedAccounts = () => {
+  cron.schedule("*/5 * * * *", async () => {
+    try {
+      const thirtyMinutesAgo = new Date(Date.now() - 30 * 60 * 1000);
+      const result = await User.deleteMany({
+        accountVerified: false,
+        createdAt: { $lt: thirtyMinutesAgo },
+      });
+      console.log(`Deleted ${result.deletedCount} unverified accounts.`);
+    } catch (error) {
+      console.error("Error deleting unverified accounts:", error);
+    }
+  });
+};
